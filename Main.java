d2fcfd008d77bbd790a0d94cf741059b7879aeb3
givingdonation <gitlab@iscienceworld.com>
2023-05-17 18:09:54 -0700

    maybe fully implemented

--- Main.java
+++ Main.java
@@ -1,4 +1,4 @@
-
+import java.util.function.Function;
 /*interface Monad {
     static <T, V extends Monad> V mReturn(T t);
     static <T, V extends Monad, Va extends Monad> V mBind(Va ma, mOper<Monad, T> aTmb);
@@ -14,28 +14,37 @@
     }
 }*/
 
-//interface mOper <T, V> {
-//    public V f (T t);
-//}
 public class Main{
 
     public static void main(String[] args) {
-        Mayba<Integer> maybeInt = new Mayba<Integer>(false, (Integer) 1);
+        //Maybe<Integer> maybeInt = new Maybe<Integer>(false, (Integer) 1);
         
-        Mayba<Integer> maybeInt2 = Mayba.mReturn(1);
-        System.out.println(maybeInt2.isNothing == maybeInt.isNothing);
+        final Maybe<Integer> maybeInt2 = Maybe.mReturn(null);
+
+        final Maybe<Integer> maybeInt3 = Maybe.mBind(maybeInt2, x -> Maybe.mReturn(x + 1));
+        
+        System.out.println(maybeInt3.value);
     }
 }
 
-class Mayba <T> {
-    public static <A> Mayba<A> mReturn (A a){
-        return new Mayba<A> (false, a);
+class Maybe <T> {
+    public static <A> Maybe<A> mReturn (A a){
+        return new Maybe<A> (a);
+    }
+    public static <A, B> Maybe<B> mBind (Maybe<A> ma, Function<A, Maybe<B>> f){
+        final Maybe<B> newMb;
+        if (ma.value == null){
+            newMb = new Maybe<B>(null);
+        } else {
+            newMb = new Maybe<B>(f.apply(ma.value).value);
+        }
+        return newMb;
     }
 
-    public boolean isNothing;
-    public T value;
-    public Mayba (boolean itIsNothing, T itsValue){
-        isNothing = itIsNothing;
+    //final public boolean isNothing;
+    final public T value;
+    public Maybe (T itsValue){
+        //isNothing = itIsNothing;
         value = itsValue;
     }
 }
